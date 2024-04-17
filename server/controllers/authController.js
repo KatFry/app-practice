dc59f0461f3d7f23cbe999c1a1b9e8a56799c323
Katherine Fry <katgfry@me.com>
2024-04-17 18:16:19 -0400

    authController: setCookie pseudocode

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,6 +3,22 @@ const authController = {};
 // require Message from the messageModel 
 const Message = require('../models/messageModel.js');
 
+// implement setCookie middleware that comes after a successful post request 
+authController.setCookie = async (req, res,  next) => {
+  // destructure the password as the req.body
+
+  // within the try block....
+  try {
+    // assign the cookie a key of pass and a value of the passed-in password 
+
+    // invoke and return next
+
+  // within the catch block...
+  } catch {
+    // invoke the global error handler, with log, status and message properties 
+
+  }
+};
 
 
 
