841a8280b0817ab71ea8f86774a109feecddb21d
Katherine Fry <katgfry@me.com>
2024-04-17 17:36:07 -0400

    working on initial messageController functionality

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,2 +1,15 @@
 // require the message model from the correct file
 const Message = require('../models/messageModel.js');
+
+
+module.exports = {
+  // add postMessage middleware to create a new item in the database
+
+
+
+  // add getMessages middleware to retrieve all items from the database and send them back as JSON
+
+
+  // add deleteMessage middleware to find items in database based on ID number and delete message if it exists
+
+};
