2198d666f05a338e7aa8eab5db8fb84c6ffb6ceb
Katherine Fry <katgfry@me.com>
2024-04-17 18:10:01 -0400

    starting authController

---
+++ server/controllers/authController.js
@@ -0,0 +1,10 @@
+// declare a const authController assigned to an empty object 
+const authController = {};
+// require Message from the messageModel 
+const Message = require('../models/messageModel.js');
+
+
+
+
+// export the authController
+module.exports = authController;
