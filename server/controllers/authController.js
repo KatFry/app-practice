a31bea46dc4e3a13281b9d2723bcbff378de177a
Katherine Fry <katgfry@me.com>
2024-04-17 18:35:38 -0400

    authController: checkCookie code finished

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -14,7 +14,7 @@ authController.setCookie = async (req, res,  next) => {
     // invoke and return next
     return next();
   // within the catch block...
-  } catch {
+  } catch (err) {
     // invoke the global error handler, with log, status and message properties 
     return next({
       log: 'An error occurred in authController.setCookie middleware',
@@ -25,21 +25,29 @@ authController.setCookie = async (req, res,  next) => {
 };
 
 // implement checkCookie middleware that comes after a user deletes a message
-authController.setCookie = async (req, res, next) => {
+authController.checkCookie = async (req, res, next) => {
   // destructure password to be req.cookies and id to be req.params
-
+  const { pass } = req.cookies;
+  const { id } = req.params;
   // if no password is given, return a 400 error and 'password required' message
-
+  if (!pass) return res.status(400).json({ error: 'password required' });
   // within try block...
   try {
     // declare a const message assigned to awaiting finding the message by its id 
-
+    const message = Message.findById({'id':id});
     // if the message's password doesn't match passed-in password, return an error 'invalid credentials'
-
+    if (message.password !== pass) {
+      return res.status(400).json({ error: 'invalid credentials' });
+    }
     // invoke and return next otherwise 
-
-  } catch {
-    // 
+    return next();
+  } catch (err) {
+    // invoke the global error handler 
+    return next({
+      log: 'An error occurred in authController.checkCookie middleware',
+      status: 400,
+      message: { err: 'An error occurred during authentication' },
+    });
   }
 };
 
