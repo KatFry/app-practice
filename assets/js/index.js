6850f584d353c2087a1489019761c13639847b02
Katherine Fry <katgfry@me.com>
2024-04-19 17:11:02 -0400

    index.js: starting getMessages function

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+// add an eventListener for after the DOM content loads 
+document.addEventListener('DOMContentLoaded', () => {
+  // declare getMessages as an async function using try and catch blocks and fetch 
+  const getMessages = async () => {
+    // within the try block...
+    try {
+      //
+      
+    // within the catch block...
+    } catch (err) {
+      //
+    }
+  };
+});
