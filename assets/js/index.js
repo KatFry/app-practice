0d4bc8d72161070a4d38658457aaac6d01140dd5
Katherine Fry <katgfry@me.com>
2024-04-19 17:59:21 -0400

    index.js: deleteButton finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,86 +24,92 @@ document.addEventListener('DOMContentLoaded', () => {
           listItem.id = message._id;
 
           // add a deleteButton property assigned to creating a 'button' element 
-          
+          const deleteButton = document.createElement('button');
           // setAttributes of button ('class', 'del') and textContent of 'Delete'
-
-          // append the deleteButtonto the listItem and the listItem to the messageList
-
+          deleteButton.setAttribute('class', 'del');
+          deleteButton.textContent = 'Delete';
+          // append the deleteButton to the listItem and the listItem to the messageList
+          listItem.appendChild(deleteButton);
+          messageList.appendChild(listItem);
           // add deleteButton eventListener functionality ( on click)
-
+          deleteButton.addEventListener('click', async () => {
             // console log 'delete button clicked for id: ', message._id for testing
-
+            console.log('delete button clicked for id: ', message._id);
             // invoke deleteMessage, passing in the id 
-
+            deleteMessage(message._id);
             // assign listItem to the getElementById method, passing in message._id
-
+            const listItem = document.getElementById(message._id);
             // if the listItem exists,
-
+            if (listItem) {
               // remove it
-
+              listItem.remove();
               // log 'list item removed from the DOM'
+              console.log('list item removed from the DOM.');
+            }
+          });
         }
-        
-      })
-        
+      });
+  
     // within the catch block...
     } catch (err) {
       // log 'error occurred in getMessages' and the error 
+      console.log('error occurred in getMessages', err);
     }
-
-    // declare a const saveButton assigned to using querySelector to find the id of save 
-
-    // add an event listener to the saveButton functionality (on click)
-
-      // add in e.preventDefault() - look up what this does 
-
-      // declare a const password assigned to using querySelector to find the id of pass and its value
-
-      // declare a const newMessage assigned to using querySelector to find the id of desc and its value 
-
-      // if either the password or newMessage are empty fields, return 
-
-      // declare a const msg assigned to passing in message and password 
-
-      // use a try/catch block to save the message 
-
-        // declare a const response assigned to awaiting the fetch to /messages
-
-          // method is POST request
-
-          // headers are Content-type. application/json
-
-          // body is JSON.stringify the msg 
-
-        // declare a const data assigned to awaiting the response converted to json 
-
-        // invoke getMessages 
-
-        // reassign values of pass and desc to empty strings 
-      
-      // in the catch block...
-
-        // console log the error 
-
-    // declare an async function deleteMessage that takes the id 
-
-      // log 'invoking deleteMessage function with id:', id 
-
-      // try catch block
-
-        // declare a const response assigned to awaiting a fetch to /messages/${id}
-
-          // method is 'DELETE'
-
-          // headers are Content-type application/json
-
-        // if the response is ok, invoke getMessages 
-
-      // catch the error
-
-        // log the error 
-  
   };
+
+  // declare a const saveButton assigned to using querySelector to find the id of save 
+
+  // add an event listener to the saveButton functionality (on click)
+
+    // add in e.preventDefault() - look up what this does 
+
+    // declare a const password assigned to using querySelector to find the id of pass and its value
+
+    // declare a const newMessage assigned to using querySelector to find the id of desc and its value 
+
+    // if either the password or newMessage are empty fields, return 
+
+    // declare a const msg assigned to passing in message and password 
+
+    // use a try/catch block to save the message 
+
+      // declare a const response assigned to awaiting the fetch to /messages
+
+        // method is POST request
+
+        // headers are Content-type. application/json
+
+        // body is JSON.stringify the msg 
+
+      // declare a const data assigned to awaiting the response converted to json 
+
+      // invoke getMessages 
+
+      // reassign values of pass and desc to empty strings 
+      
+    // in the catch block...
+
+      // console log the error 
+
+  // declare an async function deleteMessage that takes the id 
+
+    // log 'invoking deleteMessage function with id:', id 
+
+    // try catch block
+
+      // declare a const response assigned to awaiting a fetch to /messages/${id}
+
+        // method is 'DELETE'
+
+        // headers are Content-type application/json
+
+      // if the response is ok, invoke getMessages 
+
+    // catch the error
+
+      // log the error 
+  
+  
   
   // invoke getMessages
 
